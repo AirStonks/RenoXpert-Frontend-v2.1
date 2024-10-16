4aff5d1b44eb8a7765f3ded4087ac8c2688dfaec
AirStonks <changyongkang1001@gmail.com>
2024-10-16 10:06:39 +0800

    update auth api url

--- src/services/SessionManager.ts
+++ src/services/SessionManager.ts
@@ -2,7 +2,7 @@
 
 import axios from 'axios';
 
-const API_URL = 'http://' + window.location.hostname + ':8000/api/';
+const API_URL = 'https://api.renoxpert.my/api/';
 
 export class SessionManager {
     static async checkSession(mobile: string, orderId: string): Promise<boolean> {
