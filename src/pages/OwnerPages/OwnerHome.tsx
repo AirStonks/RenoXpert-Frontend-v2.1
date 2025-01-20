419e66aab277f0c1376169d9fef8177ffefd2dc0
AirStonks <changyongkang1001@gmail.com>
2025-01-20 09:42:01 +0800

    Fix owner home

--- src/pages/OwnerPages/OwnerHome.tsx
+++ src/pages/OwnerPages/OwnerHome.tsx
@@ -283,41 +283,43 @@ function OwnerHome() {
                 </div> */}
 
                 <div className="flex flex-wrap gap-4">
-                    {renoProgresses.map((progress, index) => (
-                        <Link
-                            // to={'/owner/reno-progress/' + renoProgress.id}
-                            to={'/owner/reno/progress/' + progress.id}
-                            className="card w-full sm:w-[calc(50%-0.5rem)] cursor-pointer"
-                            key={index}
-                        >
-                            <div className="card-body flex justify-between items-center">
-                                <div className="flex items-center gap-4">
-                                    <div className="relative size-[50px] shrink-0">
-                                        <svg className="w-full h-full stroke-info-clarity fill-info-light" fill="none" height="48" viewBox="0 0 44 48" width="44" xmlns="http://www.w3.org/2000/svg">
-                                            <path d="M16 2.4641C19.7128 0.320509 24.2872 0.320508 28 2.4641L37.6506 8.0359C41.3634 10.1795 43.6506 14.141 43.6506 18.4282V29.5718C43.6506 33.859 41.3634 37.8205 37.6506 39.9641L28 45.5359C24.2872 47.6795 19.7128 47.6795 16 45.5359L6.34937 39.9641C2.63655 37.8205 0.349365 33.859 0.349365 29.5718V18.4282C0.349365 14.141 2.63655 10.1795 6.34937 8.0359L16 2.4641Z" fill="#EFF6FF">
-                                            </path>
-                                            <path d="M16.25 2.89711C19.8081 0.842838 24.1919 0.842837 27.75 2.89711L37.4006 8.46891C40.9587 10.5232 43.1506 14.3196 43.1506 18.4282V29.5718C43.1506 33.6804 40.9587 37.4768 37.4006 39.5311L27.75 45.1029C24.1919 47.1572 19.8081 47.1572 16.25 45.1029L6.59937 39.5311C3.04125 37.4768 0.849365 33.6803 0.849365 29.5718V18.4282C0.849365 14.3196 3.04125 10.5232 6.59937 8.46891L16.25 2.89711Z" stroke="#1B84FF" strokeOpacity="0.2">
-                                            </path>
-                                        </svg>
-                                        <div className="absolute leading-none left-2/4 top-2/4 -translate-y-2/4 -translate-x-2/4">
-                                            <i className="ki-outline ki-tablet-text-down text-1.5xl ps-px text-info"></i>
+                    {renoProgresses.length > 0 &&
+                        renoProgresses.map((progress, index) => (
+                            <Link
+                                // to={'/owner/reno-progress/' + renoProgress.id}
+                                to={'/owner/reno/progress/' + progress.id}
+                                className="card w-full sm:w-[calc(50%-0.5rem)] cursor-pointer"
+                                key={index}
+                            >
+                                <div className="card-body flex justify-between items-center">
+                                    <div className="flex items-center gap-4">
+                                        <div className="relative size-[50px] shrink-0">
+                                            <svg className="w-full h-full stroke-info-clarity fill-info-light" fill="none" height="48" viewBox="0 0 44 48" width="44" xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M16 2.4641C19.7128 0.320509 24.2872 0.320508 28 2.4641L37.6506 8.0359C41.3634 10.1795 43.6506 14.141 43.6506 18.4282V29.5718C43.6506 33.859 41.3634 37.8205 37.6506 39.9641L28 45.5359C24.2872 47.6795 19.7128 47.6795 16 45.5359L6.34937 39.9641C2.63655 37.8205 0.349365 33.859 0.349365 29.5718V18.4282C0.349365 14.141 2.63655 10.1795 6.34937 8.0359L16 2.4641Z" fill="#EFF6FF">
+                                                </path>
+                                                <path d="M16.25 2.89711C19.8081 0.842838 24.1919 0.842837 27.75 2.89711L37.4006 8.46891C40.9587 10.5232 43.1506 14.3196 43.1506 18.4282V29.5718C43.1506 33.6804 40.9587 37.4768 37.4006 39.5311L27.75 45.1029C24.1919 47.1572 19.8081 47.1572 16.25 45.1029L6.59937 39.5311C3.04125 37.4768 0.849365 33.6803 0.849365 29.5718V18.4282C0.849365 14.3196 3.04125 10.5232 6.59937 8.46891L16.25 2.89711Z" stroke="#1B84FF" strokeOpacity="0.2">
+                                                </path>
+                                            </svg>
+                                            <div className="absolute leading-none left-2/4 top-2/4 -translate-y-2/4 -translate-x-2/4">
+                                                <i className="ki-outline ki-tablet-text-down text-1.5xl ps-px text-info"></i>
+                                            </div>
+                                        </div>
+                                        <div className="flex flex-col gap-1">
+                                            <h3 className="text-gray-900 text-sm font-medium">
+                                                {progress.property.block}-{progress.property.floor}-{progress.property.unit_no}
+                                            </h3>
+                                            <div className="flex">
+                                                <span>{progress.property.name}</span>
+                                            </div>
                                         </div>
                                     </div>
-                                    <div className="flex flex-col gap-1">
-                                        <h3 className="text-gray-900 text-sm font-medium">
-                                            {progress.property.block}-{progress.property.floor}-{progress.property.unit_no}
-                                        </h3>
-                                        <div className="flex">
-                                            <span>{progress.property.name}</span>
-                                        </div>
+                                    <div className="status">
+                                        {(((progress.pre_reno_completion * 0.2) + (progress.reno_completion * 0.7) + (progress.post_reno_completion * 0.1)) * 100).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 })}% Completed
                                     </div>
                                 </div>
-                                <div className="status">
-                                    {(((progress.pre_reno_completion * 0.2) + (progress.reno_completion * 0.7) + (progress.post_reno_completion * 0.1)) * 100).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 })}% Completed
-                                </div>
-                            </div>
-                        </Link>
-                    ))}
+                            </Link>
+                        ))
+                    }
                 </div>
             </div>
         </div>
