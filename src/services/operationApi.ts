7c769b58acda8f8dc88355ace6878c4b9d17284a
AirStonks <changyongkang1001@gmail.com>
2025-02-06 16:35:40 +0800

    fix

--- src/services/operationApi.ts
+++ src/services/operationApi.ts
@@ -244,7 +244,7 @@ export const uploadTaskDocuments = async (renoProgressId: number, taskId: number
             formData,
             {
                 headers: {
-                    'Authorization': `Bearer ${localStorage.getItem('token')}`,
+                    'Authorization': `Bearer ${localStorage.getItem('p_token')}`,
                     'Content-Type': 'multipart/form-data', // Axios sets the proper boundary for this type
                 }
             }
