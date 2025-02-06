2f73d6dc853b2c0adbf6c34fe4eafc0a3f4e0b42
AirStonks <changyongkang1001@gmail.com>
2025-02-06 16:41:39 +0800

    fix

--- src/services/operationApi.ts
+++ src/services/operationApi.ts
@@ -283,8 +283,8 @@ export const liveUploadTaskAttachment = async (renoProgressId: number, taskId: n
 
     } catch (error) {
         // Handle errors like 401 or other server-side errors
-        // handleOperation401Error(error as AxiosError);
-        // throw error; // Rethrow the error for further handling
+        handleOperation401Error(error as AxiosError);
+        throw error; // Rethrow the error for further handling
     }
 }
 
