cebcdc878b63f94c73f256d35030614d69d40420
AirStonks <changyongkang1001@gmail.com>
2025-02-07 10:22:04 +0800

    fix toggle on quotation order package

--- src/pages/Order/EditOrderQuotation.tsx
+++ src/pages/Order/EditOrderQuotation.tsx
@@ -628,30 +628,24 @@ function EditOrderQuotation() {
                                                                     </div>
                                                                 </td>
                                                                 <td className='text-center text-lg'>
-                                                                    {product.pivot.visibility ?
-                                                                        <>
-                                                                            <button
-                                                                                ref={qtyBtnRef}
-                                                                                data-action='decrease'
-                                                                                onClick={product.pivot.included ? () => adjustQuantity(product.id, prodPackage.id, 'decrease') : null}
-                                                                                disabled={!product.pivot.included}
-                                                                            >
-                                                                                <i className="ki-solid ki-minus-squared"></i>
-                                                                            </button>
-                                                                            <span className="mx-2 text-base">
-                                                                                {product.pivot.included ? ((!product.pivot.includeSupply && !product.pivot.includeInstall ? 0 : product.pivot.quantity)) : '0'}
-                                                                            </span>
-                                                                            <button
-                                                                                data-action='increase'
-                                                                                onClick={product.pivot.included ? () => adjustQuantity(product.id, prodPackage.id, 'increase') : null}
-                                                                                disabled={!product.pivot.included}
-                                                                            >
-                                                                                <i className="ki-solid ki-plus-squared"></i>
-                                                                            </button>
-                                                                        </>
-                                                                        :
-                                                                        ""
-                                                                    }
+                                                                    <button
+                                                                        ref={qtyBtnRef}
+                                                                        data-action='decrease'
+                                                                        onClick={product.pivot.included ? () => adjustQuantity(product.id, prodPackage.id, 'decrease') : null}
+                                                                        disabled={!product.pivot.included}
+                                                                    >
+                                                                        <i className="ki-solid ki-minus-squared"></i>
+                                                                    </button>
+                                                                    <span className="mx-2 text-base">
+                                                                        {product.pivot.included ? ((!product.pivot.includeSupply && !product.pivot.includeInstall ? 0 : product.pivot.quantity)) : '0'}
+                                                                    </span>
+                                                                    <button
+                                                                        data-action='increase'
+                                                                        onClick={product.pivot.included ? () => adjustQuantity(product.id, prodPackage.id, 'increase') : null}
+                                                                        disabled={!product.pivot.included}
+                                                                    >
+                                                                        <i className="ki-solid ki-plus-squared"></i>
+                                                                    </button>
 
                                                                 </td>
                                                                 <td className="text-center">
