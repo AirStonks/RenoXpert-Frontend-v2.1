6918d5680389dd05ac6077c7bf49dd9e22a54fa3
AirStonks <changyongkang1001@gmail.com>
2025-02-12 10:06:46 +0800

    hide summary

--- src/pages/Order/OrderDetail.tsx
+++ src/pages/Order/OrderDetail.tsx
@@ -1187,6 +1187,8 @@ function OrderDetail() {
 
                                                     {/* Summary */}
                                                     {packageCategories.some(category => category.category !== "undefined") &&
+                                                        orderDetail.final_amount !== null &&
+                                                        orderDetail.final_amount !== 0 &&
                                                         <div className="card-body p-4 bg-gray-50 border-l-4 border-purple-500 rounded-lg shadow-sm mb-4">
                                                             <div className="flex flex-col gap-5">
                                                                 {/* Header */}
@@ -1462,7 +1464,7 @@ function OrderDetail() {
                                         {/* Summary */}
                                         {packageCategories.some(category => category.category !== "undefined") &&
                                             orderDetail.final_amount !== null &&
-                                            orderDetail.final_amount !== 0 && 
+                                            orderDetail.final_amount !== 0 &&
                                             <div className="card-body p-4 bg-gray-50 border-l-4 border-purple-500 rounded-lg shadow-sm mb-4">
                                                 <div className="flex flex-col gap-5">
                                                     {/* Header */}
