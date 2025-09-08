1512e0280e1cb6d8c650f813e4ef766b1b802251
AirStonks <changyongkang1001@gmail.com>
2025-09-08 15:01:35 +0800

    minor fix

--- src/pages/Order/OrderDetail.tsx
+++ src/pages/Order/OrderDetail.tsx
@@ -1635,7 +1635,7 @@ function OrderDetail() {
                                                                             </span>
                                                                         </div>
                                                                         <div className="flex items-center justify-end gap-2 text-sm text-gray-600">
-                                                                            {orderDetail.is_be_powered ? (
+                                                                            {orderDetail.is_be_powered || orderDetail.is_rnpl ? (
                                                                                 <span>
                                                                                     RM{" "}
                                                                                     {prodPackage.markup_amount.toLocaleString(undefined, {
@@ -1657,7 +1657,7 @@ function OrderDetail() {
                                                                             <span>x</span>
                                                                             <span>{prodPackage.quantity || 1} (Qty)</span>
                                                                             <span>=</span>
-                                                                            {orderDetail.is_be_powered ? (
+                                                                            {orderDetail.is_be_powered || orderDetail.is_rnpl ? (
                                                                                 <span className="font-semibold text-lg text-gray-800">
                                                                                     RM{" "}
                                                                                     {(prodPackage.markup_amount * (prodPackage.quantity || 1)).toLocaleString(undefined, {
